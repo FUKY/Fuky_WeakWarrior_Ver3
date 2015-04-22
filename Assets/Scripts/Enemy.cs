9342db4c05aeaa675cb448c5d8bad7477f1f7669
khangnhfuky <khangnhfuky@users.noreply.github.com>
2015-04-23 04:40:44 +0700

    Dùng frontCheck cho Enemy1

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+    private float speed = 2f;
+    private Rigidbody2D rb2d;
+    private Animator animatorEnemy;
+    public int HP;
+
+    private int attackEnemyState;
+
+    private Transform frontCheck;
+    private bool attacked;
+
+    void Start()
+    {
+        animatorEnemy = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        frontCheck = transform.Find("frontCheck").transform;
+        rb2d.velocity = new Vector2(speed, 0f);
+        attackEnemyState = Animator.StringToHash("attackEnemy");
+
+    }
+
+    void FixedUpdate()
+    {
+        Collider2D[] frontHits = Physics2D.OverlapPointAll(frontCheck.position);
+        foreach (Collider2D col in frontHits)
+        {
+            //Enemy1 chạm và đánh Player
+            if (col.tag == "Player")
+            {
+                animatorEnemy.SetTrigger(attackEnemyState);
+                rb2d.velocity = new UnityEngine.Vector2(0f, 0f);
+            }
+        }
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //void Update()
+    //{
+    //    //Enemy1 bị chém trúng
+    //    if (HP <= 0)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+
+    void Hurt()
+    {
+        HP--;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            animatorEnemy.SetTrigger(attackEnemyState);
+            rb2d.velocity = new Vector2(0f, 0f);
+        }
+        if (col.tag == "DeathArea")
+        {
+            Hurt();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Animator animatorPlayer;
+    public RuntimeAnimatorController[] listAnimator;
+    public int state;
+    static int attackPlayerState;
+
+    public GameObject deathArea;
+
+    public bool attack;
+
+    void Start()
+    {
+        animatorPlayer = GetComponent<Animator>();
+        animatorPlayer.runtimeAnimatorController = listAnimator[state];
+        attackPlayerState = Animator.StringToHash("attackPlayer");
+    }
+    
+    public void FlipLeft()
+    {
+        animatorPlayer.SetTrigger(attackPlayerState);
+        Vector3 theScale = transform.localScale;
+        theScale.x = -1;
+        transform.localScale = theScale;        
+    }
+    public void FlipRight()
+    {
+        animatorPlayer.SetTrigger(attackPlayerState);
+        Vector3 theScale = transform.localScale;
+        theScale.x = 1;
+        transform.localScale = theScale;
+    }
+
+    public void AttackOn()
+    {
+        attack = true;
+        deathArea.SetActive(attack);
+    }
+
+    public void AttackOff()
+    {
+        attack = false;
+        deathArea.SetActive(attack);
+    }
+}
