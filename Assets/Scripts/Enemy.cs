9180488df31cc348d3db70072cbd3f611a6dc4ae
khangnhfuky <khangnhfuky@users.noreply.github.com>
2015-04-23 04:50:58 +0700

    Bỏ frontCheck chỉ dùng 1 BoxCollider2D -> tối ưu hơn (KO cần getcomponent trong FixedUpdate())

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,45 +9,45 @@ public class Enemy : MonoBehaviour {
 
     private int attackEnemyState;
 
-    private Transform frontCheck;
+    //private Transform frontCheck;
     private bool attacked;
 
     void Start()
     {
         animatorEnemy = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
-        frontCheck = transform.Find("frontCheck").transform;
+        //frontCheck = transform.Find("frontCheck").transform;
         rb2d.velocity = new Vector2(speed, 0f);
         attackEnemyState = Animator.StringToHash("attackEnemy");
 
     }
 
-    void FixedUpdate()
-    {
-        Collider2D[] frontHits = Physics2D.OverlapPointAll(frontCheck.position);
-        foreach (Collider2D col in frontHits)
-        {
-            //Enemy1 chạm và đánh Player
-            if (col.tag == "Player")
-            {
-                animatorEnemy.SetTrigger(attackEnemyState);
-                rb2d.velocity = new UnityEngine.Vector2(0f, 0f);
-            }
-        }
-        if (HP <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    //void Update()
+    //void FixedUpdate()
     //{
-    //    //Enemy1 bị chém trúng
+    //    Collider2D[] frontHits = Physics2D.OverlapPointAll(frontCheck.position);
+    //    foreach (Collider2D col in frontHits)
+    //    {
+    //        //Enemy1 chạm và đánh Player
+    //        if (col.tag == "Player")
+    //        {
+    //            animatorEnemy.SetTrigger(attackEnemyState);
+    //            rb2d.velocity = new UnityEngine.Vector2(0f, 0f);
+    //        }
+    //    }
     //    if (HP <= 0)
     //    {
     //        Destroy(gameObject);
     //    }
     //}
+
+    void Update()
+    {
+        //Enemy1 bị chém trúng
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Hurt()
     {
