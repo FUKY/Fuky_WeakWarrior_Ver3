69f959a2408a823f043180cf8af3e6228028368a
khangnhfuky <khangnhfuky@users.noreply.github.com>
2015-04-25 02:15:13 +0700

    Animation Enemy2

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    private Canvas buttons;
+
+	void Start () 
+    {
+        buttons = GameObject.FindObjectOfType<Canvas>();
+	}
+	
+}
